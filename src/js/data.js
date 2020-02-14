06d92e60a80f80e3e49a74b8cce88889962a0a83
Scottd_h <scott.herrington@me.com>
2020-02-14 11:58:27 +0000

    cleaned up details view

---
+++ src/js/data.js
@@ -0,0 +1,28 @@
+var moo =  [
+ {
+    isin: "XS1973630889",
+    name: "ZHPRHK 8 13/20 21/01/2023",
+    country_of_risk: "CHN",
+    bb_composite: "B",
+    industry_group: "Financials",
+    coupon_rate: 8.65,
+    maturity_date: "2023-01-21"
+    currency: "USD",
+    issuer_name: "Zhenro Properties Group Limited",
+    industry_sector: "Corporate"
+
+ }
+]
+            
+    
+     
+
+ 
+
+ 
+var getExpirySpan = function(securityObj){
+    var today = new Date.now;
+    console.log(today);
+},
+
+getExpirySpan();
