a0ba2f1dbc7722908d7d4e8f231a082cc93bd370
Scottd_h <scott.herrington@me.com>
2020-02-26 17:30:19 +0000

    Added add ISIN field

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,63 +1,68 @@
 const KatanaIndexLogo =
-  '<img src="./img/katana_logo_inIndex.png" style="height: 16px;" />'
+  '<img src="./img/katana_logo_inIndex.png" style="height: 16px;" />';
 
 const navItems = [
   {
     icon: null,
-    label: 'Dashboard',
-    artboard_ID: 'dashboard'
+    label: "Dashboard",
+    artboard_ID: "dashboard"
   },
   {
     icon: null,
-    label: 'Trade ideas',
-    artboard_ID: 'tradeIdeas'
+    label: "Trade ideas",
+    artboard_ID: "tradeIdeas"
   },
   {
     icon: null,
-    label: 'Bookmarks',
-    artboard_ID: 'bookmarks'
+    label: "Bookmarks",
+    artboard_ID: "bookmarks"
+  },
+  {
+    icon: null,
+    label: "Monitor",
+    artboard_ID: "monitor"
   }
-]
+];
 
 const getExpirySpan = security => {
-  var today = new Date.now()
-  console.log(security.maturity_date - today)
-}
+  var today = new Date.now();
+  console.log(security.maturity_date - today);
+};
 
 const getRandomZscore = () => {
-  var min = 1
-  var max = 4
-  var random = Math.random() * (max - min) + min
-  var zScore = random.toFixed(2)
-  return zScore
-}
+  var min = 1;
+  var max = 4;
+  var random = Math.random() * (max - min) + min;
+  var zScore = random.toFixed(2);
+  return zScore;
+};
 
 const getRandomReversion = () => {
-  var min = -20
-  var max = 100
-  var random = Math.floor(Math.random() * (max - min) + min)
-  return random
-}
+  var min = -20;
+  var max = 100;
+  var random = Math.floor(Math.random() * (max - min) + min);
+  return random;
+};
 
 const getCounts = () => {
-  const rows = [...document.querySelectorAll('.ideaRow')]
+  const rows = [...document.querySelectorAll(".ideaRow")];
 
   return {
     numIdeas: rows.length,
-    numIndexed: rows.filter(row => row.classList.contains('isIndexed')).length,
-    numNew: rows.filter(row => row.classList.contains('isNew')).length
-  }
-}
+    numIndexed: rows.filter(row => row.classList.contains("isIndexed")).length,
+    numNew: rows.filter(row => row.classList.contains("isNew")).length
+  };
+};
 
-const STORAGE_PREFIX = 'KATANA_APP'
+const STORAGE_PREFIX = "KATANA_APP";
 
 const getLocal = name =>
-  JSON.parse(window.localStorage.getItem(`${STORAGE_PREFIX}_${name}`))
+  JSON.parse(window.localStorage.getItem(`${STORAGE_PREFIX}_${name}`));
 const setLocal = (name, value) =>
   window.localStorage.setItem(
     `${STORAGE_PREFIX}_${name}`,
     JSON.stringify(value)
-  )
+  );
 
 export {
   getExpirySpan,
@@ -69,4 +74,4 @@ export {
   STORAGE_PREFIX,
   getLocal,
   setLocal
-}
+};
